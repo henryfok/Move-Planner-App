a58b2e7c3732a458c98840ee72754222f03cf17e
Henry <himleungfok@hotmail.com>
2018-10-27 23:32:52 -0700

    Add Google Maps

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ var $greeting = $('#greeting');
 var streetStr = '';
 var cityStr = '';
 
+var map;
+var geocoder;
+
 var geoLocated = false;
 var autocomplete;
 var componentForm = {
@@ -43,6 +46,8 @@ function loadData() {
 
 	loadStreetView();
 	loadNYT();
+	loadMap();
+	geocodeAddress(geocoder, map);
 	loadWiki();
 
 	return false;
@@ -125,6 +130,30 @@ function loadStreetView() {
 	var streetViewImg = '<img class="bgimg" src="' + streetViewURL + '">';
 
 	$body.append(streetViewImg);
+}
+
+function loadMap() {
+	$("#map").css("height", "300px")
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 14
+	});
+	geocoder = new google.maps.Geocoder();
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+	var address = streetStr + ', ' + cityStr;
+	geocoder.geocode({'address': address}, function(results, status) {
+	if (status === 'OK') {
+		resultsMap.setCenter(results[0].geometry.location);
+		var marker = new google.maps.Marker({
+		map: resultsMap,
+		position: results[0].geometry.location
+	});
+	} else {
+		alert('Geocode was not successful for the following reason: ' + status);
+	}
+	});
 }
 
 function loadNYT() {
